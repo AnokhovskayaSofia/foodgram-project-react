12aaa0be57615331af415ade80f8a0580782a87e
Софья <sanokhovskaya@gmail.com>
2021-10-07 14:06:22 +0300

    Serv

--- backend/api/api/settings.py
+++ backend/api/api/settings.py
@@ -10,9 +10,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
