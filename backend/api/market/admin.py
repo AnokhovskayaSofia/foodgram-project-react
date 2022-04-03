d31d24916643a73ee9a75c695d63871711085571
Софья <sanokhovskaya@gmail.com>
2022-04-03 20:11:51 +0300

    New app Marketing

--- backend/api/market/admin.py
+++ backend/api/market/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from import_export.admin import ImportMixin
 
-# Register your models here.
+from market.models import (Item, Product)
+from market.resources import (ProductResource, ItemResource)
+
+
+class ProductAdmin(ImportMixin, admin.ModelAdmin):
+    resource_class = ProductResource
+
+class ItemAdmin(ImportMixin, admin.ModelAdmin):
+    resource_class = ItemResource
