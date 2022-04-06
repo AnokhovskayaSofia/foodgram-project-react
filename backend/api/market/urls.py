b2a04d62dfc4a47f5b361b02df8ec69bb5a6569d
Софья <sanokhovskaya@gmail.com>
2022-04-06 11:13:55 +0300

    Fix urls.py

--- backend/api/market/urls.py
+++ backend/api/market/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import ProductViewSet
+from .views import ProductViewSet, ItemViewSet
 
 router = DefaultRouter()
 router.register('product', ProductViewSet, basename='ProductViewSets')
