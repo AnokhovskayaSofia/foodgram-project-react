50b2b78cebe0a7669bd71c07a737c4b2d8bba276
Софья <sanokhovskaya@gmail.com>
2022-04-06 09:56:54 +0300

    Model shopping admin and filter for Product

--- backend/api/market/views.py
+++ backend/api/market/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from rest_framework import filters, status, viewsets
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ReadOnlyModelViewSet
