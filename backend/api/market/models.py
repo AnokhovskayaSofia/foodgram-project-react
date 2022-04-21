46fe54d7034efeaa41a42b6fa9f1d11e6fc405b1
Софья <sanokhovskaya@gmail.com>
2022-04-22 00:38:41 +0300

    New model MyProduct

--- backend/api/market/models.py
+++ backend/api/market/models.py
@@ -4,22 +4,22 @@ from django.db import models
 from users.models import User
 
 
-# class Item(models.Model):
-#     name = models.CharField(
-#         max_length=200,
-#         verbose_name='Название ингредиента')
-#     color = models.CharField(
-#         max_length=7,
-#         verbose_name='Цветовой код ингредиента')
-#     slug = models.SlugField(
-#         verbose_name='Ингредиент продукта')
+class Item(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название ингредиента')
+    color = models.CharField(
+        max_length=7,
+        verbose_name='Цветовой код ингредиента')
+    slug = models.SlugField(
+        verbose_name='Ингредиент продукта')
 
-#     class Meta:
-#         verbose_name = 'Ингредиент продукта'
-#         verbose_name_plural = 'Ингредиенты продукта'
+    class Meta:
+        verbose_name = 'Ингредиент продукта'
+        verbose_name_plural = 'Ингредиенты продукта'
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
 
 
 # class MyProduct(models.Model):
